e39cf9514251cd30aa25dfb7e200302143b7d72d
Tejasva Khandelwal <tejasva@Tejasvas-MacBook-Pro.local>
2023-12-24 03:57:00 +0530

    Done with text part of hero section

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
+// HTML Elements
+// Navigation
 const navBtnEl = document.querySelector(".nav-bar-btn");
 const headerEl = document.querySelector(".nav-bar");
 
+// Functionality
+// Navigation
 navBtnEl.addEventListener("click", function () {
   headerEl.classList.toggle("nav-open");
-  console.log("clicked");
 });
