68153307ea498cf2bf0a13b7b7b623e81ef99857
Tejasva Khandelwal <tejasva@Tejasvas-MacBook-Pro.local>
2023-12-24 16:05:20 +0530

    Hero section comleted not responsive yet

--- script.js
+++ script.js
@@ -1,10 +1,21 @@
+// *****************
 // HTML Elements
+// *****************
 // Navigation
-const navBtnEl = document.querySelector(".nav-bar-btn");
-const headerEl = document.querySelector(".nav-bar");
+const navBtnEl = document.querySelector('.nav-bar-btn');
+const headerEl = document.querySelector('.nav-bar');
 
+// Accordian
+const accordianEl = document.querySelectorAll('.accordian-box');
+
+// *****************
 // Functionality
+// *****************
 // Navigation
-navBtnEl.addEventListener("click", function () {
-  headerEl.classList.toggle("nav-open");
+navBtnEl.addEventListener('click', function () {
+  headerEl.classList.toggle('nav-open');
+});
+// Accordian
+accordianEl.forEach(val => {
+  val.addEventListener('click', function () {});
 });
